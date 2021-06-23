6e9dac47dc294067e316a3eba13d463f7df1e0a5
Akkun3704 <75048902+Akkun3704@users.noreply.github.com>
2021-06-23 14:16:28 +0700

    Update nhentaipdf.js

--- plugins/nhentaipdf.js
+++ plugins/nhentaipdf.js
@@ -3,7 +3,9 @@ let axios = require('axios')
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'Cari apa?'
   let res = await axios.get('http://lolhuman.herokuapp.com/api/nhentai/'+text+'?apikey=NinoWangyy')
+  let data = await axios.get('http://lolhuman.herokuapp.com/api/nhentaipdf/'+text+'?apikey=NinoWangyy')
   let { title_romaji, title_native, info, image } = res.data.result
+  let { pdfnya } = data.data.result
   let caption = `
 ${title_romaji}
 
@@ -29,8 +31,8 @@ Uploaded : ${info.uploaded}
 `
   conn.sendFile(m.chat, image[0], null, caption.trim(), m, false, {thumbnail: Buffer.alloc(0)})
   
-  let data = await axios.get('http://lolhuman.herokuapp.com/api/nhentaipdf/'+text+'?apikey=NinoWangyy')
-  conn.sendFile(m.chat, data.data.result, title_romaji, null, m, {asDocument: true, thumbnail: image[0]})
+
+  conn.sendFile(m.chat, pdfnya, title_romaji.pdf, null, m, {asDocument: true})
 }
 handler.help = ['nhentaipdf <query>']
 handler.tags = ['downloader']
