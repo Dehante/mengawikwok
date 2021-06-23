e4217b0f0e817a6210fb870346565f01cf0fd748
Akkun3704 <75048902+Akkun3704@users.noreply.github.com>
2021-06-23 10:37:33 +0700

    Update subreddit.js

--- plugins/subreddit.js
+++ plugins/subreddit.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Media tidak ditemukan!'
-  await conn.sendFile(m.chat, json.url, text, json.title, m)
+  await conn.sendFile(m.chat, json.url, text, json.title, m, false, {thumbnail: Buffer.alloc(0)})
 }
 handler.help = ['subreddit <query>']
 handler.tags = ['internet']
